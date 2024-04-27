c395cfe389cea3812a37d19d4e697af7994df3d0
devops <dev0pslearning2025@gmail.com>
2024-04-27 13:49:18 +0530

    Daily-Usage-Scripts

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ DATE=$(date +%F)
 if [ -f ${BACKUP_DIR}/${FILE_NAME}-${DATE}.tar.gz  ]
 then
     echo "file is already exists"
-    ls -lrth ${BACKUP_DIR}/${FILE_NAME}-${DATE}.tar.gz
+    ls -1 ${BACKUP_DIR}/${FILE_NAME}-${DATE}.tar.gz
 else 
     echo "Backup file is not there, Taking the backup"
     tar -cvzf ${BACKUP_DIR}/${FILE_NAME}-${DATE}.tar.gz $FILE_NAME
