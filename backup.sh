769441685d82a49d26f9c151eea33f845d297e06
devops <dev0pslearning2025@gmail.com>
2024-04-27 13:41:57 +0530

    Daily-Using_Scripts

--- backup.sh
+++ backup.sh
@@ -6,9 +6,10 @@ FILE_PATH=/tmp/ram
 FILE_NAME=websphere
 
 if [ -d ${BACKUP_DIR}/${FILE_NAME}-${DATE}.tar.gz  ]
+then
     echo "file is already exists"
     ls -lrth ${BACKUP_DIR}/${FILE_NAME}-${DATE}.tar.gz
 else 
-    echo "Backup file is not there, Takinf the backup"
+    echo "Backup file is not there, Taking the backup"
     tar -cvzf ${BACKUP_DIR}/${FILE_NAME}-${DATE}.tar.gz $FILE_NAME
 fi
